2d46dfe89d06d88802f14c99f2fa69bab74f79c1
Max <96414062+Maksymilian-Orlowski@users.noreply.github.com>
2024-09-08 13:51:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Car;
+
+
+class Program { 
+
+    static void Main(string[] args)
+    {
+
+        Console.ReadKey();
+    }
+
+    public class Car
+    {
+        String model;
+        int year;
+        String manufacturer;
+        int numberOfDoors;
+        String color;
+
+        public Car(String model, int year, String manufacturer, int numberOfDoors, String color)
+        {
+            this.model = model;
+            this.year = year;
+            this.manufacturer = manufacturer;
+            this.numberOfDoors = numberOfDoors;
+            this.color = color;
+            
+
+        }
+
+        Car Lambo = new Car("Aventador", 2021, "Lamborghini", 2, "Blue");
+        Car Porsche = new Car("911 Carrera", 2021, "Porsche", 2, "Red");
+
+    }
+
+}
